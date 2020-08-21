b1042e9b2e0f84aa5c6b88b46651b9f57306db90
Barret Rennie <barret@brennie.ca>
2020-08-20 21:31:45 -0400

    Refactor Handle into a wrapper type

--- fxrunner/src/lib/osapi/handle.rs
+++ fxrunner/src/lib/osapi/handle.rs
@@ -2,45 +2,69 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::ffi::c_void;
 use std::ptr::null_mut;
 
 use winapi::um::handleapi;
 use winapi::um::winnt::HANDLE;
 
-// A wrapper around a HANDLE that automatically closes.
-pub struct Handle(HANDLE);
+/// A HANDLE that is closed when dropped.
+pub type Handle = AutoClosingHandle<c_void>;
 
-impl Handle {
-    /// Create a new null handle.
+pub struct AutoClosingHandle<T>(*mut T)
+where
+    *mut T: ClosableHandle;
+
+/// A trait representing a raw resource handle that can be closed.
+pub trait ClosableHandle {
+    /// Close the handle.
+    fn close(&mut self);
+}
+
+impl ClosableHandle for HANDLE {
+    fn close(&mut self) {
+        if !self.is_null() {
+            let rv = unsafe { handleapi::CloseHandle(*self) };
+            assert!(rv != 0);
+        }
+    }
+}
+
+impl<T> Drop for AutoClosingHandle<T>
+where
+    *mut T: ClosableHandle,
+{
+    fn drop(&mut self) {
+        self.0.close()
+    }
+}
+
+impl<T> AutoClosingHandle<T>
+where
+    *mut T: ClosableHandle,
+{
+    /// Create a new handle.
     pub fn null() -> Self {
-        Handle(null_mut())
+        Self(null_mut())
     }
 
-    /// Return the underlying `HANDLE`.
-    pub fn as_ptr(&self) -> HANDLE {
+    /// Return the underlying handle.
+    pub fn as_ptr(&self) -> *mut T {
         self.0
     }
 
-    /// Return a mutable pointer to the underlying `HANDLE`, allowing it to be
-    /// used as an output parameter.
-    pub fn as_out_ptr(&mut self) -> *mut HANDLE {
-        &mut self.0 as *mut HANDLE
+    /// Return a mutable double pointer to the underlying handle, allowing it to
+    /// be used as an output parameter.
+    pub fn as_out_ptr(&mut self) -> *mut *mut T {
+        &mut self.0 as *mut *mut T
     }
 }
 
-impl From<HANDLE> for Handle {
-    fn from(h: HANDLE) -> Self {
-        Handle(h)
-    }
-}
-
-impl Drop for Handle {
-    fn drop(&mut self) {
-        if !self.0.is_null() {
-            let rv = unsafe { handleapi::CloseHandle(self.0) };
-            assert!(rv != 0);
-
-            self.0 = null_mut();
-        }
+impl<T> From<*mut T> for AutoClosingHandle<T>
+where
+    *mut T: ClosableHandle,
+{
+    fn from(handle: *mut T) -> Self {
+        Self(handle)
     }
 }
